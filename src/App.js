3acebbf516092ead36b2003d947d0005158c1297
Vanessa Stewart <vstewart1213@gmail.com>
2021-02-24 20:58:31 -0800

    Add withRouter HOC to menu item for basic navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import  { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => {
+  return ( 
+    <h1>Hats Page</h1>
+   );
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
