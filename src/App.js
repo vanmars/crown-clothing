e2ceeb60eb777cf4200580eaf8878ebbb7ce549c
Vanessa Stewart <vstewart1213@gmail.com>
2021-02-24 21:55:30 -0800

    Add styling to shop page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 import  { Route, Switch } from 'react-router-dom';
-
-const HatsPage = () => {
-  return ( 
-    <h1>Hats Page</h1>
-   );
-}
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import CollectionItem from '../collection-item/collection-item.component';
+import './preview-collection.styles.scss';
+
+const PreviewCollection = ({ title, items }) => {
+  return ( 
+    <div className='preview-collection'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <div className='preview'>
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(({id, ...otherItemProps}) => (
+            <CollectionItem key={id} {...otherItemProps} />
+          ))
+        }
+
+      </div>  
+    </div>
+
+   );
+}
+ 
+export default PreviewCollection;
