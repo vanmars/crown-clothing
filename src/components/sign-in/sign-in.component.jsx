3b7e914e4a2cbc6e28a007e0023e4811316ebb82
Vanessa Stewart <vstewart1213@gmail.com>
2021-02-25 18:06:58 -0800

    Add form input and custom button components for sign in form

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -44,7 +45,9 @@ class SignIn extends React.Component {
             label='Password'
             required 
           />
-          <input type="submit" value='Submit Form' />
+          <CustomButton type="submit">
+            Sign In
+          </CustomButton>
         </form>
 
       </div>
