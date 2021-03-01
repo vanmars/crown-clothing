0771c6f3f95cc3c6112b2839fca9583104ee5f2e
Vanessa Stewart <vstewart1213@gmail.com>
2021-02-28 16:21:41 -0800

    Add user selectors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import  { createStructuredSelector } from 'reselect';
+
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors.js';
+import { selectCurrentUser} from '../../redux/user/user.selectors.js';
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
@@ -37,9 +41,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({ user: { currentUser}, cart: {hidden} }) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({ 
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
