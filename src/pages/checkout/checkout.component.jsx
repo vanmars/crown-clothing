7d61ca8553b419fdecc6a27a7919e2bfb5c6bc03
Vanessa Stewart <vstewart1213@gmail.com>
2021-02-28 16:33:26 -0800

    Set up routing for new checkout page component

---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './checkout.styles.scss';
+
+const CheckoutPage = () => {
+  return ( 
+    <div className='checkout'>
+      CheckoutPage
+    </div>
+   );
+}
+ 
+export default CheckoutPage;
