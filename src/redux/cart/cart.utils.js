898d768c5990ff25b6c37576eb966737b025c502
Vanessa Stewart <vstewart1213@gmail.com>
2021-03-01 12:31:47 -0800

    Install redux-persist library to persist cart items in local storage

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,15 +4,15 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   );
 
   if (existingCartItem) {
-    return cartItems.map(cartItem => 
-      cartItem.id === cartItemToAdd.id 
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
     );
   }
-  
-  return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
-}
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   const existingCartItem = cartItems.find(
@@ -20,12 +20,12 @@ export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   );
 
   if (existingCartItem.quantity === 1) {
-    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
   }
 
   return cartItems.map(cartItem =>
-    cartItem.id === cartItemToRemove.id 
-      ? {...cartItems, quantity: cartItem.quantity - 1}
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
       : cartItem
   );
-}
+};
