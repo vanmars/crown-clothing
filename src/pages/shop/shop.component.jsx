bf63b13955f933cce1018388eb69f90966ae3141
Vanessa Stewart <vstewart1213@gmail.com>
2021-03-01 16:49:29 -0800

    Transfer shop data to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import SHOP_DATA from './shop.data.js';
+import { selectCollections } from '../../redux/shop/shop.selectors';
 import PreviewCollection from '../../components/preview-collection/preview-collection.component';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      collections : SHOP_DATA
-    }
-  }
-
-  render() { 
-    const {collections} = this.state;
-
-    return ( 
-      <div className='shop-page'>
-        {
-          collections.map(({id, ...otherCollectionProps}) => (
-            <PreviewCollection key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-     );
-  }
+const ShopPage = ({ collections }) => {
+  return ( 
+    <div className='shop-page'>
+      {
+        collections.map(({id, ...otherCollectionProps}) => (
+          <PreviewCollection key={id} {...otherCollectionProps} />
+        ))
+      }
+    </div>
+    );
 }
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+})
  
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
