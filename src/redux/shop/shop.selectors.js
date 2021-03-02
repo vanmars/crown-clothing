e4fa40288fd023b96db17262e101fe9916fdf13f
Vanessa Stewart <vstewart1213@gmail.com>
2021-03-01 17:35:13 -0800

    Fix bug in shop selector

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -16,10 +16,9 @@ export const selectCollections = createSelector(
   shop => shop.collections
 )
 
-export const selectCollection = memoize((collectionUrlParam)
+export const selectCollection = memoize((collectionUrlParam) =>
   createSelector(
     [selectCollections],
-    collections => collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+    (collections) => collections[collectionUrlParam]
   )
+);
